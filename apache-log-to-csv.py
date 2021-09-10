c0d5b1844b252744ad2b41f26e9980abd2f579a0
Leuconoe <n.leuconoe@gmail.com>
2021-09-10 11:53:56 +0900

    Update and rename __init__.py to apache-log-to-csv.py
    working on python 3.8

--- apache-log-to-csv.py
+++ apache-log-to-csv.py
@@ -28,7 +28,6 @@ import csv
 import apache_log_parser
 import argparse
 
-
 class Colors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -36,39 +35,50 @@ class Colors:
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
-
-
+    
 def main(**kwargs):
-
+#def main():
+    bcolors = Colors()
+    #kwargs = {}
+    #kwargs['format'] = "%a %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\""
+    #kwargs['input'] = 'apache.log'
+    #kwargs['output'] = kwargs['input']+'.csv'
     print('Converting, please wait...')
-
     line_parser = apache_log_parser.make_parser(kwargs['format'])
+    ######################
+    #test = '100,200,100,200 - - [07/Sep/2021:06:25:07 +0000] "GET https://abcde.com HTTP/1.1" 403 498 "-" "Mozilla/5.0 Safari/537.36"'
+    #line_parser = apache_log_parser.make_parser("%a %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"")
+    #log_line_data = line_parser(test)
+    #print(log_line_data)
+    ######################
     header = True
-
-    with open(kwargs['input'], 'rb') as inFile, open(kwargs['output'], 'w') as outFile:
-
+    with open(kwargs['input'], 'rb') as inFile, open(kwargs['output'], 'w', newline='') as outFile:
         lines = inFile.readlines()
-        writer = csv.writer(outFile, delimiter=';')
-
+        writer = csv.writer(outFile)
         for line in lines:
-            try:
-                log_line_data = line_parser(line)
-            except apache_log_parser.LineDoesntMatchException as ex:
-                print(bcolors.FAIL + 'The format specified does not match the log file. Aborting...' + bcolors.ENDC)
-                print('Line: ' + ex.log_line + 'RegEx: ' + ex.regex)
-                exit()
-
-            if header:
-                writer.writerow(list(log_line_data.keys()))
-                header = False
-            else:
-                writer.writerow(list(log_line_data.values()))
-
+            if line and len(line)>4:
+                line = line.decode().replace('\n','')
+                try:
+                    log_line_data = line_parser(line)
+                except apache_log_parser.LineDoesntMatchException as ex:
+                    print(bcolors.FAIL + 'The format specified does not match the log file. Aborting...' + bcolors.ENDC)
+                    print('Line: ' + ex.log_line + 'RegEx: ' + ex.regex)
+                    return
+                except TypeError as ex:
+                    print(bcolors.FAIL + 'TypeError. Aborting...' + bcolors.ENDC)
+                    print(line)
+                    return
+                
+                if header:
+                    writer.writerow(log_line_data.keys())
+                    header = False     
+                
+                writer.writerow(log_line_data.values())                
+                
     print(bcolors.OKGREEN + 'Conversion finished.' + bcolors.ENDC)
-
-
+        
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Convert Apache logs to csv', version='%(prog)s 1.0')
+    parser = argparse.ArgumentParser(description='Convert Apache logs to csv')
     parser.add_argument('format', type=str, help='Apache log format (see http://httpd.apache.org/docs/2.2/logs.html)')
     parser.add_argument('input', type=str, help='Input log file ex. /var/log/apache/access.log')
     parser.add_argument('output', type=str, help='Output csv file ex. ~/accesslog.csv')
